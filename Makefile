375d54208cc0bdbbd4523ec39a244f9714838ac3
Rahulkumar Gayatri <rgayatri@lbl.gov>
2023-03-14 11:20:22 -0700

    Updated main file to include CUDA .

--- Makefile
+++ Makefile
@@ -1,21 +1,31 @@
 # A simple Makefile to build both mpihello w/o cuda and cudahello with cuda calls.
-CC=CC
+CC=clang++
 CFLAGS= -O3
 
-#CFLAGS += -DDEBUG
+CFLAGS += -DDEBUG
 
+ifeq ($(cuda), y)
+	CC=nvcc -ccbin CC -m64 --x cu
+	CFLAGS += -DCUDA
+	CFLAGS += -I/global/cfs/cdirs/m1759/rgayatri/cuda-samples/Common/
+endif
 ifeq ($(openacc), y)
 	CFLAGS += -acc
 endif
 ifeq ($(openmp), y)
-	CFLAGS += -mp
+#	CFLAGS += -mp
+	CFLAGS += -fopenmp
 endif
 ifeq ($(openmp_target), y)
-	CFLAGS += -mp=gpu -DOPENMP_TARGET
+#	CFLAGS += -mp=gpu -DOPENMP_TARGET
+	CFLAGS +=  --offload-arch=sm_80 -DOPENMP_TARGET
+	CFLAGS += -I$(CRAY_MPICH_PREFIX)/include
+	LDFLAGS += -L$(CRAY_MPICH_PREFIX)/lib -lmpi
+	LDFLAGS += $(PE_MPICH_GTL_DIR_nvidia80) $(PE_MPICH_GTL_LIBS_nvidia80)
 endif
 
-main.ex: main.cpp
-	$(CC) $(CFLAGS) -o $@ main.cpp $(LINKFLAGS)
+main.ex: main.cpp 
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ main.cpp $(LINKFLAGS)
 
 clean:
 	rm -f *.exe *.ex *.o
--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include <chrono>
+#include <mpi.h>
+#include <sched.h>
+
+#if CUDA
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <helper_cuda.h>
+#endif
 
 using namespace std::chrono;
 
@@ -8,7 +16,38 @@ void random_ints(int *a, int N) {
     for (i = 0; i < N; ++i) a[i] = rand();
 }
 
+#if CUDA
+__global__ void kernel(int N, int *a, int *b, int *c)
+{
+    const int index = blockIdx.x * blockDim.x + threadIdx.x; 
+    if(index < N)
+        c[index] = a[index] + b[index];
+}
+#endif
+
 int main(int argc, char **argv) {
+    int myid, namelen, world_size;
+    char myname[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    MPI_Get_processor_name(myname, &namelen);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    char* lrank = getenv("SLURM_PROCID");
+
+    printf("Lrank from MPI = %s", lrank);
+
+    char my_gpu[15];
+
+    fprintf(stdout,
+            "Hello Rahul from processor %s, rank = %d out of %d processors"
+            "\n",
+            myname, myid, world_size);
+
+    // synchronize so the loop guarantees to prints all the information of one
+    // rank before progressing.
+    MPI_Barrier(MPI_COMM_WORLD);
 
     const int N = argc > 1 ? atoi(argv[1]) : 10;
 
@@ -21,11 +60,26 @@ int main(int argc, char **argv) {
     random_ints(a, N);
     random_ints(b, N);
 
+#if CUDA
+    int *d_a, *d_b, *d_c;
+    checkCudaErrors(cudaMalloc(&d_a, N * sizeof(int)));
+    checkCudaErrors(cudaMalloc(&d_b, N * sizeof(int)));
+    checkCudaErrors(cudaMalloc(&d_c, N * sizeof(int)));
+
+    checkCudaErrors(cudaMemcpy(d_a, a, N * sizeof(int), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_b, b, N * sizeof(int), cudaMemcpyHostToDevice));
+#endif
+
     time_point<std::chrono::system_clock> start, end;
 
     start = system_clock::now();
     // do work
-#if _OPENMP
+#if CUDA
+    const int threads = 32;
+    const int blocks = N/threads + 1;
+    kernel<<<blocks,threads>>> (N, d_a, d_b, d_c);
+#else
+#if defined(_OPENMP)
 #if OPENMP_TARGET
 #pragma omp target teams distribute parallel for \
     map(to: a[:N], b[:N]) map(from: c[:N])
@@ -35,6 +89,7 @@ int main(int argc, char **argv) {
 #elif _OPENACC
 #pragma acc parallel loop gang vector \
     copyin(a[:N], b[:N]) copyout(c[:N])
+#endif
 #endif
     for (int i = 0; i < N; i++) 
         c[i] = a[i] + b[i];
@@ -54,6 +109,17 @@ int main(int argc, char **argv) {
     delete[] a;
     delete[] b;
     delete[] c;
+
+#if CUDA
+    checkCudaErrors(cudaFree(d_a));
+    checkCudaErrors(cudaFree(d_b));
+    checkCudaErrors(cudaFree(d_c));
+#endif
+
+    fprintf(stdout,
+            "\n****************************************************************"
+            "******************** \n");
+    MPI_Finalize();
 
     return 0;
 }
